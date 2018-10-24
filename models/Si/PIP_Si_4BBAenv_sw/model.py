a290b80c1dfa8c5b6f56c0746551d121c8701aa7
Cas van der Oord <casv2@cam.ac.uk>
2018-10-24 09:44:25 +0100

    sw2

--- models/Si/PIP_Si_4BBAenv_sw/model.py
+++ models/Si/PIP_Si_4BBAenv_sw/model.py
@@ -3,7 +3,7 @@ import julip
 import os
 
 model_dir = os.path.dirname(os.path.realpath(__file__))
-IP = juliaimport.import_IP(model_dir + "/SiPIP_4BBA_env_sw.json")
+IP = juliaimport.import_IP(model_dir + "/SiPIP_4BBA_env_sw2.json")
 
 #IP = juliaimport.import_IP(os.path.realpath(__file__)[0:-8] + "Ti_4B_med.json")
 ASE_IP = julip.JulipCalculator("IP")
